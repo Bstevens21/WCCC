e3fb648a9033afd1e125b1b93c53bfaf1efc0072
Matt Cirkovic <matthewcirkovic@yahoo.com>
2018-02-27 08:22:01 -0700

    Revert "Changed titles"
    This reverts commit 2a750ed2a38c59784418fd5cab5988b1c1ff150d.

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Archive
+                    Laravel
                 </div>
 
                 <div class="links">
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Base
+                    Laravel
                 </div>
 
                 <div class="links">
