528314694bf06ee8783e189d7292ddc1878ee330
Matt Cirkovic <matthewcirkovic@yahoo.com>
2018-04-18 22:28:58 -0600

    added about info and fixed bottom of home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -160,7 +160,7 @@
   <!-- FOOTER -->
   <footer class="container">
     <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; 2007-2018 WCCC &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
   </footer>
 </main>
   
